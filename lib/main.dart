45d5aea5cf4f4873809f82cbb12d24fec546becd
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-15 15:51:20 +0600

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class GoRouterTest extends StatelessWidget {
  GoRoute(path: "/", builder: (context, state) => const SplashScreen(),),
    GoRoute(  name :RouteName.homepage   ,path: "/home", builder: (BuildContext context, GoRouterState state) => const HomeScreen(),),
    GoRoute(name : RouteName.chatpage ,path: "/chat", builder: (context, state) => const ChatScreen(),),
-   GoRoute(name : RouteName.profilepage,path: "/profile:data", builder: ( BuildContext context, GoRouterState state) =>  ProfieScreen(
+   GoRoute(name : RouteName.profilepage,path: "/profile", builder: ( BuildContext context, GoRouterState state) =>  ProfieScreen(
     data: state.queryParameters['data']!
    ),),
 
