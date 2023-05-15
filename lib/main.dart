f9f0ac60ac7621b2c5ff4825038c3668f7272210
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-15 12:55:00 +0600

    route config

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:go_route/screen/home_screen.dart';
+import 'package:go_router/go_router.dart';
 
 import 'screen/splash_screen.dart';
 
@@ -10,20 +12,31 @@ void main() async {
 
 
 
-  runApp(const GoRouterTest());
+  runApp( GoRouterTest());
 }
 
 class GoRouterTest extends StatelessWidget {
-  const GoRouterTest({Key? key}) : super(key: key);
+   GoRouterTest({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+       routerConfig:routefunction ,
         debugShowCheckedModeBanner: false,
         title: 'Go Router Test',
         theme: ThemeData(
           primarySwatch: Colors.deepOrange,
         ),
-        home: const SplashScreen());
+        );
   }
+
+
+
+
+
+    final GoRouter routefunction =GoRouter (routes:[
+ GoRoute(path: "/", builder: (context, state) => const SplashScreen(),),
+  GoRoute(path: "/home", builder: (context, state) => const HomeScreen(),),
+    ] );
+
 }
